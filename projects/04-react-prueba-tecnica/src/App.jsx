8b7a8b62406ff4ec2e5349b0483d1cda98b7f86a
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-10-03 21:16:40 -0300

    Add fourth project

---
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,23 @@
+import { useCatFact } from './hooks/useCatFact'
+import { useCatImage } from './hooks/useCatImage'
+import './App.css'
+
+export function App () {
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
+
+  const handleClick = () => {
+    refreshFact()
+  }
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      <button onClick={handleClick}>Get new fact</button>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words of "${fact}"`} />}
+      </section>
+    </main>
+  )
+}
---
+++ projects/04-react-prueba-tecnica/src/hooks/useCatImage.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+const CAT_IMAGE_PREFIX = 'https://cataas.com/cat'
+
+export function useCatImage ({ fact }) {
+  const [imageUrl, setImageUrl] = useState('')
+
+  useEffect(() => {
+    if (!fact) return
+
+    const threeFirstWords = fact.split(' ', 3).join('%20')
+
+    const imageEnd = `/says/${threeFirstWords}?fontSize=50&fontColor=red`
+
+    setImageUrl('')
+
+    fetch(`${CAT_IMAGE_PREFIX}${imageEnd}&json=true`)
+      .then(res => res.json())
+      .then(json => {
+        const { _id: id } = json
+        setImageUrl(`${CAT_IMAGE_PREFIX}/${id}${imageEnd}`)
+      })
+  }, [fact])
+
+  return { imageUrl }
+}
