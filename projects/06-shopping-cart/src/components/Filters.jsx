727628f294e99ebddf983b2cc14f636ba8356a9d
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-10-07 22:34:35 -0300

    Add sixth project

---
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -0,0 +1,44 @@
+import { useId, useContext } from 'react'
+import './Filters.css'
+
+import { FiltersContext } from '../context/filtersProvider'
+
+export function Filters () {
+  const minPriceFilterId = useId()
+  const categoryFilterId = useId()
+  const { filters, setFilters } = useContext(FiltersContext)
+
+  const handleChangeMinPrice = (event) => {
+    setFilters(prevState => ({
+      ...prevState,
+      minPrice: event.target.value
+    }))
+  }
+
+  const handleChangeCategory = (event) => {
+    setFilters(prevState => ({
+      ...prevState,
+      category: event.target.value
+    }))
+  }
+
+  return (
+    <section className='filters'>
+      <div>
+        <label htmlFor={minPriceFilterId}>Precio mínimo</label>
+        <input type='range' id={minPriceFilterId} min='0' max='1000' onChange={handleChangeMinPrice} />
+        <span>${filters.minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor={categoryFilterId}>Categoría</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
+          <option value='all'>Todas</option>
+          <option value='laptops'>Portátiles</option>
+          <option value='smartphones'>Celulares</option>
+        </select>
+      </div>
+
+    </section>
+  )
+}
---
+++ projects/06-shopping-cart/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { FiltersProvider } from './context/filtersProvider.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+    <FiltersProvider>
+      <App />
+    </FiltersProvider>
+
+  </StrictMode>
+)
